129ed27150a2fd86bb6ccfb8cc6fe251482f8ca3
Muir Halleron <muir.halleron@gmail.com>
2017-07-05 17:28:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/takeinventory/EditorActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.takeinventory;
+
+import android.app.Activity;
+
+/**
+ * TakeInventory Created by Muir on 05/07/2017.
+ */
+
+public class EditorActivity extends Activity {
+}
